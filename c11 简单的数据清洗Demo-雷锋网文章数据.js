bfa895d8beb07f7a482d1d3588a957d7537f0501
ShenJianShou <admin@bihu.mobi>
2018-02-08 17:46:22 +0800

    Update c11 简单的数据清洗Demo-雷锋网文章数据.js

--- c11 简单的数据清洗Demo-雷锋网文章数据.js
+++ c11 简单的数据清洗Demo-雷锋网文章数据.js
@@ -1,13 +1,19 @@
-/**
-* 该清洗应用主要的功能是对虎嗅网文章的爬取结果进行一定的处理，支持原生JavaScript
-* 请在神箭手云上运行代码：http://docs.shenjian.io/overview/guide/develop/cleaner.html
+/*
+  神箭手云_数据清洗开发示例代码
+  该数据清洗应用主要的功能是对虎嗅网文章的爬取结果进行处理
+  
+  该应用依赖Demo中的<简单的文章爬虫Demo-雷锋网文章>
+  1.先导入<简单的文章爬虫Demo-雷锋网文章>，运行爬虫获取爬取数据
+  2.创建一个清洗应用，拷贝代码到清洗应用中
+  3.在清洗应用的设置中选择输入数据为雷锋网文章爬虫的爬取数据，同时设置输出数据的位置
+  4.启动清洗
+  
+  开发语言：原生JavaScript
+  开发教程：http://docs.shenjian.io/develop/cleaner/doc/concept.html
+  请在神箭手云上运行代码：http://docs.shenjian.io/overview/guide/develop/cleaner.html
+*/
 
-* 该应用依赖Demo中的<简单的文章爬虫Demo-雷锋网文章>
-* 1.请先导入<简单的文章爬虫Demo-雷锋网文章>
-* 2.点击运行爬虫
-* 3.在设置中将当前清洗应用的输入数据选择为该爬虫的爬取数据
-* 4.启动清洗
-**/
+// 首先通过configs定义输出数据的字段（json格式）
 var configs = {
   fields: [
     {
@@ -22,7 +28,7 @@ var configs = {
       name: "article_publish_time",
       required: true
     },
-    //删除了爬虫中的作者(article_author)字段
+    //和清洗前的数据字段相比，去掉了爬虫中的作者(article_author)字段
     {
       //该字段为新增字段
       name:"article_from"
